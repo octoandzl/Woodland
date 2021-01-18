fc82f04ad9dffb14c10c8bde813d9b7faea6f13c
Aadil <andzlhub@gmail.com>
2021-01-18 23:44:45 +0100

    implemented Tree

--- Tree.py
+++ Tree.py
@@ -3,6 +3,10 @@ from . import Node
 
 class Tree():
 
-	def __init__(self):
+	def __init__(self,Node):
 		self.root = Node
-		
+
+	@classmethod
+	def get_children_Trees(self,cls):
+		for c in self.root.children:
+			yield cls(c)
