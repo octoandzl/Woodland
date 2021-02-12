1d664a4a7b2b3c5299b07691ca72e9950f597bf1
Aadil <andzlhub@gmail.com>
2021-02-12 22:21:45 +0100

    implementation of ID3Tree

--- ID3Tree.py
+++ ID3Tree.py
@@ -1,2 +1,24 @@
 import sys
 print(sys.path)
+from . import Node
+from  . import Tree
+
+
+
+class ID3Tree():
+
+	def __init__(self,Root):
+		self.root = Root
+		self.tree = Tree(Root)
+
+	def compute_split():
+		pass
+
+	def evaluate_entropy():
+		pass
+
+	def compute_decision():
+		pass
+
+	def print_result():
+		pass
